8e4763d804c930840d216545e5aee3856e737d72
Alaa Aldandachi <58466561+AlaaDan@users.noreply.github.com>
2023-03-26 21:15:33 +0200

    Middleware added

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const carRouter = require('./routers/carRouter')
 const sinusRouter = require('./routers/sinusRouter.js')
 
 app.use(express.json())
+app.use((request, response, next) => {
+    console.log(`Middle ware before route: ${request.url} and method: ${request.method}`);
+    next();
+});
 
 app.use('/api/productslist', sinusRouter)
 app.use('/api/cart', carRouter)
 
+
+app.use((request, response, next) => {
+    response.status(404).json({ error: 'No endpoint found' });
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server on port ${PORT} started`)
